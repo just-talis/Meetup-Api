80500bd68490d74883e5ff1c1c513c63c83e2aa0
esther simon <esthersimon540@gmail.com>
2022-10-23 01:10:26 +0100

    name

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
- 
-export const Users = () => {
-  const [users, setUsers] = useState()
+import React, { useEffect, useState } from 'react'
 
-  function data () {
-    setUsers (users)
+export const Users = () => {
+  const [users, setUsers] = useState([])
+
+  async function getUsers() {
+    const data = await fetch('https://randomuser.me/api/?results=500').then((response) => response.json())
+    setUsers(data.results)
   }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+
   return (
-    <></>
+    <>
+      {users.map(user => (
+        <div>
+          <img src={user.picture.thumbnail} />
+          <h2>{user.name.title} {user.name.first} {user.name.last}</h2>
+          <p>Email : {user.email}</p>
+          <p>Gender : {user.gender}</p>
+          <p>Cell : {user.cell}</p>
+        </div>
+      ))}
+    </>
   )
-  
+
 }
